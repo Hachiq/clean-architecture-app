102fd15d8f12f88f86a9861c0b02772ed737aaad
Hachiq <lesiv433@gmail.com>
2024-06-26 11:20:33 +0300

    Add test for refresh token service

---
+++ UnitTests/Services/RefreshTokenServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services.DateTimeProvider;
+using Application.Services.RefreshTokenService;
+using Moq;
+
+namespace UnitTests.Services
+{
+    public class RefreshTokenServiceTest
+    {
+        [Fact]
+        public void CreateToken_ShouldReturnValidRefreshToken()
+        {
+            // Arrange
+            var mockDateTimeProvider = new Mock<IDateTimeProvider>();
+            var fixedDateTime = new DateTime(2023, 1, 1);
+            mockDateTimeProvider.Setup(x => x.UtcNow).Returns(fixedDateTime);
+
+            var refreshTokenService = new RefreshTokenService(mockDateTimeProvider.Object);
+
+            // Act
+            var refreshToken = refreshTokenService.CreateToken();
+
+            // Assert
+            Assert.NotEqual(Guid.Empty, refreshToken.Id);
+            Assert.NotEmpty(refreshToken.Token);
+            Assert.Equal(fixedDateTime, refreshToken.CreatedAt);
+            Assert.Equal(fixedDateTime.AddDays(30), refreshToken.ExpiresAt);
+        }
+    }
+}
