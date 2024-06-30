175602a061a8a035d02047bb0fe31e2c6e44b038
Hachiq <lesiv433@gmail.com>
2024-06-30 22:01:36 +0300

    Add DateTimeProvider test

---
+++ UnitTests/Services/DateTimeProviderTest.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Services;
+
+namespace UnitTests.Services
+{
+    public class DateTimeProviderTest
+    {
+        private readonly DateTimeProvider _dateTimeProvider;
+
+        public DateTimeProviderTest()
+        {
+            _dateTimeProvider = new DateTimeProvider();
+        }
+
+        [Fact]
+        public void UtcNow_ShouldReturnCurrentUtcDateTime()
+        {
+            // Arrange
+            var before = DateTime.UtcNow;
+
+            // Act
+            var result = _dateTimeProvider.UtcNow;
+
+            // Assert
+            var after = DateTime.UtcNow;
+
+            Assert.InRange(result, before, after);
+        }
+    }
+}
