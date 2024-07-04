24e8af9d5695ccd653ae5a599d299f3369caa060
Hachiq <lesiv433@gmail.com>
2024-07-04 14:50:54 +0300

    Add register service and template

--- Presentation/Client/src/app/pages/register/register.component.ts
+++ Presentation/Client/src/app/pages/register/register.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
---
+++ Presentation/Client/src/app/pages/register/services/register.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterRequest } from '../interfaces/register.request';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  public register(user: RegisterRequest): Observable<any> {
+    return this.http.post<any>(
+      `${environment.apiUrl}/auth/register`,
+      user
+    )
+  }
+}
