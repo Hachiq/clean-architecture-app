30e133a37a80d4e70443be543ecee56efa05874f
Hachiq <lesiv433@gmail.com>
2024-07-08 15:49:47 +0300

    Add user profile component

--- Presentation/Client/src/app/app-routing.module.ts
+++ Presentation/Client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: "register", component: RegisterComponent },
   { path: "login", component: LoginComponent },
   { path: "home", component: HomeComponent },
+  { path: "user-profile/:id", component: UserProfileComponent }
 ];
 
 @NgModule({
